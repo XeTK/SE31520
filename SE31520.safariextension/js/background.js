adc2e2f85faf667d1417a9df269ed98ec09c65b3
Tom Rosier <tom.rosier92@gmail.com>
2014-11-27 21:49:01 +0000

    commented some more of the code

--- SE31520.safariextension/js/background.js
+++ SE31520.safariextension/js/background.js
@@ -9,23 +9,30 @@
 
 var background = background || {}
 
+// This holds the old list of broadcasts.
 background.last  = {};
 
+// Path to the RESTful interface.
 background.url   = "http://127.0.0.1:3000/rest/extensions.json";
 
+// The key in local storage.
 background.key   = 'feeds';
 
-background.debug = true;
+background.debug = false;
 
-
+// Method that is ran when the application is loaded and will setup the application.
 background.init = function() {
 
+	// Reload the previous list broadcasts that has been presented.
 	background.last = LocalStorage.load(this.key);
 
+	// Load the user info from local storage.
 	var user = LocalStorage.load('user');
 
+	// This authenticates the user with the REST toolkit libary.
 	RESTToolKit.auth(user.name, user.pwd);
 
+	// Create a alarm to rerun this module and check for updates.
 	chrome.alarms.create(
 		"Check Rest Connection", 
 		{
@@ -35,14 +42,18 @@ background.init = function() {
 	);
 }
 
+// This creates a Chrome notification that is viewable by the operating system.
 background.notification = function(item) {
 
+	// This create a instantiation of the Moment.js libary.
 	var momentObj = moment(new Date(item.created));
 
 	var dateStr   = momentObj.format('HH:mm DD/MM/YY');
 
+	// Make a nice title for the application.
 	var content   = item.name + " - " + dateStr;
 
+	// Setup all of the permeters for the notifcation with the message and appropriate image.
 	var opts = {
 		type:           "basic",
 		contextMessage: content,
@@ -51,6 +62,7 @@ background.notification = function(item) {
 		iconUrl:        "./img/128.png"
 	}
 
+	// Send the notification object we created to chrome to display as a notification.
 	chrome.notifications.create(
 		content, 
 		opts,
@@ -60,44 +72,58 @@ background.notification = function(item) {
 	);
 }
 
+// This is the method that is run to check to see if any new notifications have been created.
 background.updateFeeds = function() {
 
 	if (background.debug)
 		console.log("background.updateFeeds: function run!");
 
+	// Get the latest version of the content from the RESTful interface.
 	var restReq   = RESTToolKit.get(background.url);
 
+	// Get the actual list from the request.
 	var feedsObjs = restReq.responseJSON;
 
+	// Convert the list objects to JSON strings to be more easily compared.
 	var feedsStr  = JSON.stringify(feedsObjs);
 	var last      = JSON.stringify(background.last);
 
+	// If the lists differ then we do more checks.
 	if (feedsStr != last) {
+
 		if (background.debug)
 			console.log("background.updateFeeds: object differs.");
 
+		// Do a more in detail check to verify the lists really differ and return the new elements.
 		var diffed = utils.diff(feedsObjs, background.last);
 
+		// Check to see if there are some new elements within the diffed array.
 		if (diffed.length > 0) {
 
+			// For each of the new elements add a new notification.
 			diffed.forEach(
 				function(item) {
+					// Create a new notification to the display.
 					background.notification(item);
 				}
 			);
 		}
 
+		// Save the new list of elements.
 		LocalStorage.save(feedsObjs, background.key);
+		//Update the old list.
 		background.last = feedsObjs;
 	} 
 }
 
+// When the alarm triggers then reject the feeds.
 chrome.alarms.onAlarm.addListener(
 	function(alarm) {
 		background.updateFeeds();
 	}
 );
 
+// When the application loads then trigger the update event.
 window.addEventListener(
 	"load", 
 	function() {
--- SE31520.safariextension/js/users.js
+++ SE31520.safariextension/js/users.js
@@ -8,31 +8,43 @@
 
 var users = users || {};
 
+// Define the region that the list is put into.
 users.userID  = '#users';
 
 users.debug   = false;
 
+// The base URL for the rest interfaces.
 users.baseUrl = "http://127.0.0.1:3000/users";
 
+// Create the url for getting the global list of users.
 users.url     = users.baseUrl + ".json";
 
+// Keep a count of the users so we can give them a unique ID.
 users.count   = 0;
 
+// Keep the address of the element we are currently editting.
 users.curEdit = '';
 
+// Method that is run when the page is loaded.
 users.init = function() {
 
+	// Check to see if the user has been validated.
 	var valid = utils.check4User();
 
+	// If the user details have been entered.
 	if (valid) {
 
+		// Load the user credentials from local storage.
 		var user = LocalStorage.load('user');
 
+		// Authenticate the user within the RESTToolKit API.
 		RESTToolKit.auth(user.name, user.pwd);
 
+		// Pass the information that is needed to set up pagionisation.
 		utils.listID  = '#users';
 		utils.perPage = 3;
 
+		// Override the load content segment, to allow us to load each user.
 		utils.loadContent = function(item) {
 			users.user(item);
 		};
@@ -40,14 +52,17 @@ users.init = function() {
 		if (this.debug)
 			console.log("users.init: Starting.");
 
+		// Load the page's content.
 		users.loadData(false);
 
+		// Bind the back button on the page to the page back in pagionisation.
 		$('#backbut').click(
 			function(){
 				utils.lastPage();
 			}
 		);
 
+		// Bind the next button to load the next page from pagionisation.
 		$('#nextbut').click(
 			function() {
 				users.loadData(false);
@@ -55,24 +70,31 @@ users.init = function() {
 			}
 		);
 
+		// When the submit button on the form is pressed then we want to submit the form via rest.
 		$('#edit_user').submit(
 			function(event) {
 
+				// Serialise the data from the form.
 				var formData = $("#edit_user").serialize();
 				
 				// CORS is having a bit of a issue with cross domain XHR's ?!?!
 				try {
+					// Create a POST object that we sent back to the CSA application.
 					RESTToolKit.postASync(
 						users.curEdit, 
-						formData,
+						formData, // The form data we got earlier.
 						function(data) {
 
+							// Reload the page with the new data.
 							users.loadData(true);
 
+							// Hide the modal window we used to enter the data in.
 							$('#basicModal').modal('hide');
 
+							// Build the persons name into a nice string to be displayed.
 							var name = $('#user_firstname').val() + ' ' + $('#user_surname').val();
 
+							// Display to the user that we have updated there data.
 							alertify.success(name + '\'s Data has been updated!');
 						}
 					);
@@ -80,6 +102,7 @@ users.init = function() {
 					console.error(err);
 				}
 
+				// Override the default behaviour that is present when submitting a form.
 				event.preventDefault();
 
 				return false;
@@ -88,13 +111,16 @@ users.init = function() {
 	}
 }
 
+// Load the various page segments.
 users.loadData = function(reload) {
 
 	if (this.debug)
 		console.log('running user.loadData()');
 
+	// Call the pagionisation method within utils to display the appropriate parts of the page.
 	utils.loadPages(this.url, reload);
 
+	// If there is less than one page then we dont want to display the pagionisation buttons.
 	if (utils.pages.length <= 1) {
 		$('#pagenumber').hide();
 		$('#buttons').hide();
@@ -103,39 +129,50 @@ users.loadData = function(reload) {
 }
 
 
-
+// Load the modal window that allows us to edit the user details.
 users.loadEdit = function(userId) {
 
+	// Build the correct url for the post request that we send this object to.
 	var baseUrl = this.baseUrl + '/' + userId;
 
+	// Hold the post url till we actually need it.
 	var postURL = baseUrl;
 
+	// Build the base url to get data about the user we are currently interagating.
 	var url     = baseUrl + '.json';
 
+	// Get the user object from the REST request.
 	var user    = RESTToolKit.get(url);
 
+	// Extract the actualy user object from the rest request. 
 	user        = user.responseJSON;
 
+	// If it is a valid object then we continue.
 	if (user) {
 
+		// Load all the appropriate data from the rest object we got back from the CSA app.
 		$('#user_firstname').val(user.firstname);
 		$('#user_surname').val(user.surname);
 		$('#user_email').val(user.email);
 		$('#user_phone').val(user.phone);
 		$('#user_grad_year').val(user.grad_year);
+
+		// Open the modal window to allow the user to edit the data.
 		$('#basicModal').modal();
 
+		// Set the currently being edited url to the global variable to make it easy to access.
 		this.curEdit = postURL;
 	}
 
 }
 
-
+// This is the method that adds new users to the form. It overwrites the class within Utils.
 users.user = function(us) {
 
 	if (this.debug)
 		console.log(JSON.stringify(us));
 
+	// This this the HTML for the user elements.
 	var html = "\
 		<div data-restid=\"" + us.url + "\" id=\"user" + this.count + "\">\
 			<h3>" + us.firstname + " " + us.surname + " - " + us.grad_year + "</h3>\
@@ -145,8 +182,10 @@ users.user = function(us) {
 		</div>\
 	";
 
+	// Append the new user to the page.
 	$(this.userID).append(html);
 
+	// This is the function that is called for when the edit button is pressed.
 	function clickEvent(userID) {
 		var inUserID = userID;
 		this.run = function() {
@@ -154,13 +193,17 @@ users.user = function(us) {
 		}
 	}
 
+	// Create a new instantiation of the clickEvent object that we defined above.
 	var runable  = new clickEvent(us.id);
 
+	// Build a variable with the element id in to pass to jQuery.
 	var buttonID = "#editbut" + us.id;
 
+	// Bind that new runable object to the button within the DOM so when it is pressed run the edit method.
 	$(buttonID).click(runable.run);
 }
 
+// When page loads then we run the initiate method.
 $(document).ready(
 	function() {
 		users.init();
